1d1bc88199545901abdd90b80317aecf621c6474
Breno Garcia <brenopgarcia@Brenos-MBP.lan>
2019-12-24 01:58:22 +0000

    Interface cloned with drag and drop

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export default function Header() {
+  return (
+    <Container>
+      <h1>Pipefy interface</h1>
+    </Container>
+  );
+}
